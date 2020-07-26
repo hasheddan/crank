a90cdbb0025f9d9a4fa7f18c603d041ff8c08b00
hasheddan <georgedanielmangum@gmail.com>
2020-07-26 16:04:31 -0500

    Create Inactive PackageRevisions in Package controller
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- pkg/controller/manager/reconciler.go
+++ pkg/controller/manager/reconciler.go
@@ -23,14 +23,16 @@ import (
 
 	"github.com/hasheddan/crank/apis/v1alpha1"
 	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
 	"github.com/crossplane/crossplane-runtime/pkg/event"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/meta"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 
 	"github.com/hasheddan/crank/pkg/dag"
@@ -46,7 +48,7 @@ const (
 
 // Reconciler reconciles packages.
 type Reconciler struct {
-	client client.Client
+	client resource.ClientApplicator
 	log    logging.Logger
 	record event.Recorder
 }
@@ -56,7 +58,10 @@ func Setup(mgr ctrl.Manager, l logging.Logger) error {
 	name := "packages/" + strings.ToLower(v1alpha1.PackageGroupKind)
 
 	r := &Reconciler{
-		client: mgr.GetClient(),
+		client: resource.ClientApplicator{
+			Client:     mgr.GetClient(),
+			Applicator: resource.NewAPIPatchingApplicator(mgr.GetClient()),
+		},
 		log:    l.WithValues("controller", name),
 		record: event.NewAPIRecorder(mgr.GetEventRecorderFor("packages")),
 	}
@@ -64,6 +69,7 @@ func Setup(mgr ctrl.Manager, l logging.Logger) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
 		For(&v1alpha1.Package{}).
+		Owns(&v1alpha1.PackageRevision{}).
 		Complete(r)
 }
 
@@ -89,55 +95,95 @@ func (r *Reconciler) Reconcile(req reconcile.Request) (reconcile.Result, error) 
 		return reconcile.Result{}, errors.Wrap(resource.IgnoreNotFound(err), "cannot get Module")
 	}
 
-	d := dag.New()
-	for _, pkg := range m.Spec.Packages {
-		if pkg.Image == p.Spec.Package {
-			p.Status.SetConditions(runtimev1alpha1.Available(), runtimev1alpha1.ReconcileSuccess())
-			return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
-		}
-		if err := d.AddNode(pkg.Image); err != nil {
-			p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
-			r.record.Event(p, event.Warning(event.Reason("failed building state"), err))
-			return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
-		}
-	}
-	for _, pkg := range m.Spec.Packages {
-		if err := d.AddEdges(map[string][]string{pkg.Image: pkg.Dependencies}); err != nil {
-			p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
-			r.record.Event(p, event.Warning(event.Reason("failed building state"), err))
-			return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
-		}
-	}
-	if err := d.AddNode(p.Spec.Package); err != nil {
+	// Construct DAG for Module.
+	d, err := dag.New(m.Spec.Packages)
+	if err != nil {
 		p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
-		r.record.Event(p, event.Warning(event.Reason("failed adding node"), err))
+		r.record.Event(p, event.Warning(event.Reason("failed building state"), err))
 		return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
 	}
-	deps, err := unpack.Unpack(p.Spec.Package)
+
+	// Unpack this image.
+	// TODO(hasheddan): no need to unpack each time.
+	digest, deps, err := unpack.Unpack(p.Spec.Package)
 	if err != nil {
 		p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
 		r.record.Event(p, event.Warning(event.Reason("failed to unpack package"), err))
 		return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
 	}
-	if err := d.AddEdges(map[string][]string{p.Spec.Package: deps}); err != nil {
+	// If node does not exist in DAG then we want to add it and make sure it is
+	// valid.
+	if !d.NodeExists(strings.Split(p.Spec.Package, ":")[0]) {
+		// If node does not exist already then adding it should always be successful.
+		if err := d.AddNode(strings.Split(p.Spec.Package, ":")[0]); err != nil {
+			p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
+			r.record.Event(p, event.Warning(event.Reason("failed adding node"), err))
+			return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
+		}
+		// Check to see if all dependencies are satisfied.
+		if err := d.AddEdges(map[string][]string{strings.Split(p.Spec.Package, ":")[0]: deps}); err != nil {
+			// If dependencies are not satisfied, we need to install them.
+			p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
+			r.record.Event(p, event.Warning(event.Reason("failed adding package dependencies"), err))
+			return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
+		}
+		// Check that adding dependencies does not result in cyclical dependency.
+		if _, err := d.Sort(); err != nil {
+			p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
+			r.record.Event(p, event.Warning(event.Reason("cyclical dependency"), err))
+			return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
+		}
+		// Append the package to the Module and attempt to update.
+		if m.Spec.Packages == nil {
+			m.Spec.Packages = map[string]v1alpha1.PackageDependencies{}
+		}
+		m.Spec.Packages[strings.Split(p.Spec.Package, ":")[0]] = v1alpha1.PackageDependencies{
+			Name:         p.Name,
+			Image:        p.Spec.Package,
+			Dependencies: deps,
+		}
+		// If another package has updated the Module since we read it then this will
+		// fail. Try again after short wait.
+		if err := r.client.Update(ctx, m); err != nil {
+			log.Debug("Cannot update package module", "error", err)
+			return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(err, "cannot update Module")
+		}
+	}
+	// If updating the Module was successful then we can create PackageRevision safely.
+	pr := &v1alpha1.PackageRevision{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   digest,
+			Labels: map[string]string{"crank.crossplane.io/package": p.Name},
+		},
+		Spec: v1alpha1.PackageRevisionSpec{
+			DesiredState: v1alpha1.PackageRevisionInactive,
+			Revision:     1,
+			Image:        p.Spec.Package,
+		},
+	}
+	meta.AddOwnerReference(pr, meta.AsController(meta.ReferenceTo(p, v1alpha1.PackageGroupVersionKind)))
+	if err := r.client.Applicator.Apply(ctx, pr, resource.MustBeControllableBy(p.UID), desiredStateApplicator()); err != nil {
+		log.Debug("Cannot create PackageRevision", "error", err)
 		p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
-		r.record.Event(p, event.Warning(event.Reason("failed adding package dependencies"), err))
-		return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
+		return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot create PackageRevision")
 	}
-	if _, err := d.Sort(); err != nil {
-		p.Status.SetConditions(runtimev1alpha1.Unavailable(), runtimev1alpha1.ReconcileSuccess())
-		r.record.Event(p, event.Warning(event.Reason("cyclical dependency"), err))
-		return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
-	}
-	m.Spec.Packages = append(m.Spec.Packages, v1alpha1.PackageDependencies{
-		Name:         p.Name,
-		Image:        p.Spec.Package,
-		Dependencies: deps,
-	})
-	if err := r.client.Update(ctx, m); err != nil {
-		log.Debug("Cannot update package module", "error", err)
-		return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(resource.IgnoreNotFound(err), "cannot update Module")
-	}
+	p.Status.CurrentRevision = digest
 	p.Status.SetConditions(runtimev1alpha1.Available(), runtimev1alpha1.ReconcileSuccess())
 	return reconcile.Result{RequeueAfter: aShortWait}, errors.Wrap(r.client.Status().Update(ctx, p), "cannot update package status")
+}
+
+// TODO(hasheddan): pretty sure there is a cleaner way to do this
+func desiredStateApplicator() resource.ApplyOption {
+	return func(_ context.Context, current, desired runtime.Object) error {
+		cr, ok := current.(*v1alpha1.PackageRevision)
+		if !ok {
+			return errors.New("not package revision")
+		}
+		dr, ok := desired.(*v1alpha1.PackageRevision)
+		if !ok {
+			return errors.New("not package revision")
+		}
+		dr.Spec.DesiredState = cr.Spec.DesiredState
+		return nil
+	}
 }
