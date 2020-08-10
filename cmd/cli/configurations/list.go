3b251f29dcb9331abf88a0362260f8e696ad0095
hasheddan <georgedanielmangum@gmail.com>
2020-08-10 17:48:57 -0500

    Add configurations CLI commands
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ cmd/cli/configurations/list.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package configurations
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hasheddan/crank/apis"
+	"github.com/hasheddan/crank/apis/v1alpha1"
+	"github.com/hasheddan/crank/pkg/prompt"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+// list will list all installed Crossplane Configurations.
+var list = &cobra.Command{
+	Use:   "list",
+	Short: "List all installed Crossplane Configurations",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		conf, err := ctrl.GetConfig()
+		if err != nil {
+			panic(err)
+		}
+		s := runtime.NewScheme()
+		if err := apis.AddToScheme(s); err != nil {
+			panic(err)
+		}
+		c, err := client.New(conf, client.Options{Scheme: s})
+		if err != nil {
+			panic(err)
+		}
+		m := &v1alpha1.PackageLock{}
+		if err := c.Get(context.TODO(), types.NamespacedName{Name: "packages"}, m); err != nil {
+			panic(err)
+		}
+		for n, p := range m.Spec.Packages {
+			fmt.Printf(prompt.FmtInfo(n))
+			fmt.Printf(prompt.FmtNotice("  📦 " + p.Image))
+			for _, d := range p.Dependencies {
+				fmt.Printf(prompt.FmtNotice(" 👉 " + d))
+			}
+		}
+	},
+}
